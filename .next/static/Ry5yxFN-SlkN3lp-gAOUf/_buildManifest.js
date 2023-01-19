e29c1bed84fbeced82d37136b5823e12d5defe6b
yahyaSurvive <putuyahya5566@gmail.com>
2023-01-19 18:52:19 +0800

    yahya build ne

---
+++ .next/static/Ry5yxFN-SlkN3lp-gAOUf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1beb14451460885a.css","static/chunks/pages/index-ca25c07d40232d88.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
